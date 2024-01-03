5ff28b0255f9bb576310ebc0b3df8269caf211df
Somash Manandhar <manandharsomash@gmail.com>
2024-01-03 21:44:44 +0545

    req/resp methods

--- 02-express/app.js
+++ 02-express/app.js
@@ -1,14 +1,38 @@
 /*
          res.writeHead(200,header)
          ->Status code : ISRCS
+
+
+         ->unlimited passing of html in .write or .end
+
 */
 
 const http =require('http')
+const fs=require('fs')
+
+const dummyData=fs.readFileSync('./dummy.html')
 
 const server=http.createServer((req,res)=>{
 
-    //request objs :
-     console.log(req.method)
+            //request objs :
+    //  console.log(req.method)
+     if(req.url==='/'){
+        // res.writeHead(200,{'content-type': 'text/html'})// yo garda html ko op ko form 
+        res.writeHead(200,{'content-type': 'text/plain'})//yo garda htm ko form ma
+        res.write(dummyData)
+        res.end()
+     }
+     else if(req.url==='/about'){
+        res.writeHead(200,'ok',{'content-type': 'text/html'})
+        res.write("this is a about page")
+        res.end()
+     }
+     else{
+        res.writeHead(404,{'content-type':'text/html'})
+        res.write("page not found")
+        res.end()
+     }
+
 
 
 
@@ -16,6 +40,8 @@ const server=http.createServer((req,res)=>{
 
 
     //resp objs
+
+    //syntax: res.writehead(statusCode,'statusMessage',{headers})
     res.writeHead(200,{'content-type': 'text/html'})//here content type is a header that specifies the browser to render certain formats ie content-type can have the value 'text/html' or 'application/json' 
     res.write('<h1>home page</h1>')
     res.end()//calls .end() explicitly
