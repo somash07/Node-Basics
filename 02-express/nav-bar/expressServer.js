b84a98a75242d0f9967435c216068be5f1da42fe
Somash Manandhar <manandharsomash@gmail.com>
2024-01-04 23:09:29 +0545

    minor change

--- 02-express/nav-bar/expressServer.js
+++ 02-express/nav-bar/expressServer.js
@@ -5,15 +5,15 @@ const app=express()
 
 //setup static and middleware
 // app.use(express.static('./public'))
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/',(req,res) => {
    res.sendFile(path.resolve(__dirname,'./index.html'))
 })
-
-
-
+//for index renderring we can just add index.html to the public.
+//SSR->later....
 app.all('*',(req,res)=>{
    res.status(404).send("resource not found")
 })
