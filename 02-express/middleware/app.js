a4b3dee3ee967f43738f8608b7960e13409b409e
Somash Manandhar <manandharsomash@gmail.com>
2024-01-07 22:53:25 +0545

    middlewares basics

--- 02-express/middleware/app.js
+++ 02-express/middleware/app.js
@@ -1,16 +1,20 @@
 /*
     Middlewares: Express apps are just a bunch of middlewares. Funx that execute during request to the server.
 
-    Reques t-->middleware--->response
+    Request-->middleware--->response
 
     middleware must be passed it onto next middleware until we are terminating the whole cycle by sending the response.send
+
+    1.app.use('route',middleware)
+    2. options: our own,built in middlewares in express,thirdparty middlewares
+
 
 */
 
 const express=require('express')
 const app= express();
 const logger=require('./logger');//importing looger fnx from another file.
-const authorization=require('./authorize')
+const authority=require('./authorize')
 
 // //middleware  , express supplies the res,req,next
 // const logger=(req,res,next)=>{
@@ -34,27 +38,29 @@ const authorization=require('./authorize')
         res.send('About')
     })
 
-    app.use(logger)//invokes logger for each and every route, order matters ie here for home and about route the logger is added manually so keeping this below.
+    // app.use(logger)//invokes logger for each and every route, order matters ie here for home and about route the logger is added manually so keeping this below.
+    
+    app.use('/api',logger)//logger for route /api
 
-    app.use([authorization,logger]) //using both middlewares, order matters
+    // app.use([authority,logger]) //using both middlewares, order matters
 
 app.get('/api/v1',(req,res)=>{
     res.send("v1")
 })
-
 app.get('/api/v2',(req,res)=>{
     res.send("v2")
 })
 
 //now for adding authorization with path /route only.we can also
 
-app.use('/route',authorization)
+app.use('/route',authority)
 
 app.get('/route/smth',(req,res)=>{
     res.send("this is a resp of route path")
 })
 
 app.get('/route/smthsmth',(req,res)=>{
+    console.log(req.user)//user data form middeware
     res.send("this is a resp of route path")
 })
 
