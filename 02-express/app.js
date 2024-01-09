23c6ff32b279d0c5646396146a6ca5603a0bceb2
Somash Manandhar <manandharsomash@gmail.com>
2024-01-09 22:13:14 +0545

    post

--- 02-express/app.js
+++ 02-express/app.js
@@ -1,10 +1,29 @@
+// const people = [
+//     { id: 1, name: 'john' },
+//     { id: 2, name: 'peter' },
+//     { id: 3, name: 'susan' },
+//     { id: 4, name: 'anna' },
+//     { id: 5, name: 'emma' },
+// ]
+
 const express=require('express')
+const {people}=require('./data.js')
+const path=require('path')
 const app=express()
-const {people}=require('./data')
 
-//get method.
-app.get('/api/peoples',(req,res)=>{
-    res.status(200).json({success: true, data: people})
+app.use(express.static(path.resolve(__dirname,'Form-httpMethods')))  //absolute path
+
+//parse form data
+app.use(express.urlencoded({extended: false})) //parses incoming req with urlencoded payloads. extended->parsing data with query string library when false and qslibrary when true
+app.post('/login',(req,res)=>{
+    console.log(req.body)
+
+    const {name}=req.body
+    if(name){
+        return res.status(200).send(`welcome ${name}`)
+        return
+    }
+    res.send('post')
 })
-app.listen(4501,()=>console.log('listening to port 4501'))
 
+app.listen(4501,()=>console.log('listening to port 4501'))
