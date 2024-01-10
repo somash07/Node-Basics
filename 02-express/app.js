ca9fd4d1f202dc8f70c131fd72a7435aa061601a
Somash Manandhar <manandharsomash@gmail.com>
2024-01-10 21:42:09 +0545

    basics

--- 02-express/app.js
+++ 02-express/app.js
@@ -7,17 +7,16 @@
 // ]
 
 const express=require('express')
-const {people}=require('./data.js')
+const {people}=require('./data')
 const path=require('path')
 const app=express()
 
 app.use(express.static(path.resolve(__dirname,'Form-httpMethods')))  //absolute path
 
 //parse form data
-app.use(express.urlencoded({extended: false})) //parses incoming req with urlencoded payloads. extended->parsing data with query string library when false and qslibrary when true
+app.use(express.urlencoded({extended: false})) //parses incoming req with urlencoded payloads based on body parser. extended->parsing data with query string library when false and qslibrary when true
 app.post('/login',(req,res)=>{
     console.log(req.body)
-
     const {name}=req.body
     if(name){
         return res.status(200).send(`welcome ${name}`)
