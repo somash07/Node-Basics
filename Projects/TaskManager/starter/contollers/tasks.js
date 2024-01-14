cbeeaecf97ceef527c02aa769a1437a058a572a3
Somash Manandhar <manandharsomash@gmail.com>
2024-01-14 23:49:37 +0545

    validations

--- Projects/TaskManager/starter/contollers/tasks.js
+++ Projects/TaskManager/starter/contollers/tasks.js
@@ -1,11 +1,13 @@
 
+const Task=require('../models/Task')
 
 const getAllTasks=(req,res) =>{
     res.send('all items are listed here')
 }
 
-const createTask=(req,res)=>{
-    res.json(req.body)
+const createTask=async(req,res)=>{
+    const task=await Task.create(req.body)
+    res.status(201).json({ task })
 }
 const deleteTask=(req,res)=>{
     res.json(req.params)
---
+++ Projects/TaskManager/starter/models/Task.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+
+const TaskSchema=new mongoose.Schema({
+    name: {
+        //inbuilt validators
+        type: String,
+        required: [true,'must provide a name'],
+        trim: true,
+        maxlength: [20,'name cannnot be more than 20  characters.']
+    },
+     completed: {
+        type: Boolean,
+        default: false,
+
+     }
+})
+
+module.exports =mongoose.model('Task',TaskSchema)
